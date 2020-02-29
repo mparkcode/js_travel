274e181bb3404b59eae7471968444a05243108d5
Michael Park <mparkcode@gmail.com>
2020-02-29 20:38:23 +0900

    displaying countr flag and name on screen on user input

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const fetchCountryData = async name => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-  return response.data
+  const response = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`);
+  return response.data;
 }
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('#dropdown');
 input.addEventListener('input', async ()=>{
+  dropdown.innerHTML = '';
   const countries = await fetchCountryData(event.target.value);
   countries.forEach(country => {
-    console.log(country);
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <img src=${country.flag} />
+      <h1>${country.name}</h1>
+    `;
+    dropdown.appendChild(div)
   })
 })
