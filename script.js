23ddcd80e7c8a2053d4ba44ad9a7a37408ed47a0
Michael Park <mparkcode@gmail.com>
2020-02-29 20:21:25 +0900

    changed fetchData function name to fetchCountyData

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const fetchData = async name => {
+const fetchCountyData = async name => {
   const response = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
   console.log(response.data)
 }
 
 const input = document.querySelector('input');
 input.addEventListener('input', ()=>{
-  fetchData(event.target.value);
+  fetchCountryData(event.target.value);
 })
