179682d0f746f06109b530916ff1e03630c41ec9
Michael Park <mparkcode@gmail.com>
2020-02-29 20:28:54 +0900

    fixed typo in fetchCountyData function, saving respoonse to variable, and iterating over variable to console.log individual countries

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const fetchCountyData = async name => {
+const fetchCountryData = async name => {
   const response = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-  console.log(response.data)
+  return response.data
 }
 
 const input = document.querySelector('input');
-input.addEventListener('input', ()=>{
-  fetchCountryData(event.target.value);
+input.addEventListener('input', async ()=>{
+  const countries = await fetchCountryData(event.target.value);
+  countries.forEach(country => {
+    console.log(country);
+  })
 })
