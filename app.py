b7bf2cbf273b994a414c4b9445650e2a737003a8
Michael Park <mparkcode@gmail.com>
2020-03-01 23:51:06 +0900

    sending photo api key through to template

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', weather_key=os.getenv('OPENWEATHER_KEY'))
+    return render_template('index.html', weather_key=os.getenv('OPENWEATHER_KEY'), photo_key=os.getenv('PIXABAY_KEY'))
 
 
 
