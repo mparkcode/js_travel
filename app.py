48b2243137a0065c8c20e9e705bf0778fa797889
Michael Park <mparkcode@gmail.com>
2020-03-01 23:12:42 +0900

    rendering index.html on running app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 
