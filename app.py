400812a1e79380b34a0a612bb7e947bb6f2f3a8c
Michael Park <mparkcode@gmail.com>
2020-03-01 23:09:19 +0900

    created app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',
+            port=int('8080'),
+            debug=True)
